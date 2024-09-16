a0460d248499bd7e85f1caa000b73b5bc96d4b18
HemantDahiyaCodes <HemnatDahiyaCodes@gmail.com>
2024-09-16 07:35:38 +0530

    created the design and the logic for the email verification

---
+++ script.js
@@ -0,0 +1,18 @@
+const input = document.getElementById('email');
+const notifyBtn = document.querySelector('button');
+const text = document.querySelector('.errorText');
+
+const verifyEmail = () => {
+    let patternOne = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(input.value === "") {
+        text.textContent = `Please enter a valid email.`
+        text.style.border = "2px solid black";
+        return false;
+    }
+
+    const valid = patternOne.test(input.value);
+    text.textContent = `Your will receive the notification when we are live. Thank you`;
+    text.style.border = "2px solid black";
+}
+
+notifyBtn.addEventListener('click', verifyEmail)
